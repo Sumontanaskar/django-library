7b2d8c755b37fc9de09944a805c78a561b2d8939
Sumontanaskar <newsumonts@gmail.com>
2017-08-08 13:31:07 +0800

    Update 1.02 08_08_2017

---
+++ testing/error_inject.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Example error_injection.sh vpc-prod-vertex03
+#
+Inject_string="192.168.10.115:9192"
+curl -G --data-urlencode "hostname=$1" --data-urlencode "log=MWBidRequestHandler::Unable to send" http://$Inject_string/myapp/Error_track/
+curl -G --data-urlencode "hostname=$1" --data-urlencode "log=WinNotify Unable to send message" http://$Inject_string/myapp/Error_track/
+curl -G --data-urlencode "hostname=$1" --data-urlencode "log=Error deploying RabbitMQ" http://$Inject_string/myapp/Error_track/
+
+ 
+
