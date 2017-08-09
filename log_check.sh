d4b18f391d9b29a65ca63e2f419c2bf14de8b665
Sumontanaskar <newsumonts@gmail.com>
2017-08-09 16:51:40 +0800

    Update 1.04 08_08_2017 log_check file argument change

--- log_check.sh
+++ log_check.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 #LIST="newsumonts@gmail.com sumonta@mobilewalla.com"
 LIST="sumonta@mobilewalla.com"
-hostname="vpc-prod-vertex03"
+hostname=`hostname`
+Log_location="/var/log/auth.log"
 server_url="http://192.168.162.128:9192/myapp/Error_track/"
-tail -fn0  /var/log/auth.log| \
+echo $hostname $server_url $LIST
+
+tail -fn0 $Log_location | \
 while read line ; do
         echo "$line" | egrep 'WinNotify Unable to send message|AMQP error for winNotify|configure exchanges are : 0 check config properties
- exchanges.path|AMQP error for bidId|Error sending msg to kafka|Error status send by mq event bus send|MWBidRequestHandler::Unable to send 
+ exchanges.path|AMQP error for bidId|Error sending msg to kafka|Error status send by mq event bus send|MWBidRequestHandler::Unable to send
 BID message|Error getting value from redis|Error deploying RabbitMQ|AMQP Deployment Error'>log_check.txt
         if [ $? = 0 ]
         then
 #               echo Prepare to send email
-#               mail -s "[Prod-Vertex-21] [ERROR] [US-EAST] [Error/Exceptions in Log File] [/home/wordster/deploy/logs/error.log]" $LIST < 
+#               mail -s "[Prod-Vertex-21] [ERROR] [US-EAST] [Error/Exceptions in Log File] [/home/wordster/deploy/logs/error.log]" $LIST <
 log_check.txt
 #               echo Email sent
                 log=$(cat "log_check.txt")
