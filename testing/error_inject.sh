b39e30840a9c72bd963d37ed6271d9a3c0d822e3
Sumontanaskar <newsumonts@gmail.com>
2017-08-12 19:38:20 +0800

    Update 1.05 12_08_2017 visual page structure update

--- testing/error_inject.sh
+++ testing/error_inject.sh
@@ -3,9 +3,14 @@
 # Example error_injection.sh vpc-prod-vertex03
 #
 Inject_string="localhost:9192"
-curl -G --data-urlencode "hostname=$1" --data-urlencode "log=MWBidRequestHandler::Unable to send BID message" http://$Inject_string/myapp/Error_track/
-curl -G --data-urlencode "hostname=$1" --data-urlencode "log=WinNotify Unable to send message" http://$Inject_string/myapp/Error_track/
-curl -G --data-urlencode "hostname=$1" --data-urlencode "log=Error deploying RabbitMQ" http://$Inject_string/myapp/Error_track/
+function curl_post() {
+	curl -G --data-urlencode "hostname=$1" --data-urlencode "log=MWBidRequestHandler::Unable to send BID message" http://$Inject_string/myapp/Error_track/
+	curl -G --data-urlencode "hostname=$1" --data-urlencode "log=WinNotify Unable to send message" http://$Inject_string/myapp/Error_track/
+	curl -G --data-urlencode "hostname=$1" --data-urlencode "log=Error deploying RabbitMQ" http://$Inject_string/myapp/Error_track/
+}
 
- 
-
+for var in "$@"
+do
+    echo "$var"
+    curl_post "$var"
+done 
